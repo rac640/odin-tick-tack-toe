93d7272ad7ed573e9652440c0eaf171bdf8952e0
rac640 <50179707+rac640@users.noreply.github.com>
2025-07-06 20:21:14 -0400

    Created factory function createPlayer()

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
 console.log("test")
+
+
+// You’re going to store the gameboard as an array 
+// inside of a Gameboard object, so start there! Your players 
+// are also going to be stored in objects, and you’re probably going 
+// to want an object to control the flow of the game itself.
+
+
+// Create a gameboard object: 
+
+function gameboard(){
+    gameboardArray = []
+}
+
+
+
+
+// Factory function to create a player object, code used from: https://www.geeksforgeeks.org/javascript/what-are-factory-functions-in-javascript/# 
+
+function createPlayer(name) {
+    return {
+        name: name,
+        talk: function () {
+            console.log('My name is '
+                + name + ', the player.');
+        }
+    };
+}
+
+const player1 = createPlayer('Player1')
