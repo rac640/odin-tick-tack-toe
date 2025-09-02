4ababcd5e9a76ba842a565a491226010f324e3ea
rac640 <50179707+rac640@users.noreply.github.com>
2025-09-01 20:41:27 -0400

    created gameBoardObject, player1, and player2 objects

---
+++ javascript.js
@@ -0,0 +1,24 @@
+
+// You’re going to store the gameboard as an array inside of a Gameboard object, so start there! 
+const gameBoardObject = {
+
+    // Making a 2D array: https://www.geeksforgeeks.org/javascript/javascript-2d-array/
+
+ gameBoardArray: 
+    ['X', 'X', '',
+    '', '', '',
+    '', '', ''],
+};
+
+
+
+//  Your players are also going to be stored in objects
+const player1 = {
+
+};
+
+const player2={
+
+};
+
+
