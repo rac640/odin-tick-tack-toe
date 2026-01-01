f0f7c5dbbc5a90ef17215d6825a04cb447a73d51
rac640 <50179707+rac640@users.noreply.github.com>
2026-01-01 00:44:37 -0500

    created player 1 and player 2 objects, and gave them name, token, and values (Player 1 is X, Player 2 is O)

--- javascript.js
+++ javascript.js
@@ -2,12 +2,13 @@
 
 
 
-
+// Step 2: Create players; give them turn properties + values. 
 
 //  created a 2D array: https://sentry.io/answers/how-can-i-create-a-two-dimensional-array-in-javascript/
 function Gameboard() {
   const my2DArray = [];
   const rows = 3;
+
   const columns = 3;
 
   for (let i = 0; i < rows; i++){
@@ -22,3 +23,69 @@ function Gameboard() {
 }
 
 
+
+
+const player1 = {
+    name: player1Name ,
+    token: 1
+}
+
+const player2 = {
+    name: player2Name, 
+    token: 2
+}
+
+
+// if the token is 1, then it is player1's turn 
+
+
+
+
+
+
+
+
+
+// below code taken from: https://replit.com/@40percentzinc/ConnectFourConsole#script.js
+// The GameController will be responsible for controlling the low and state of the game's turns, as well as whether anybody has won the game
+
+// function GameController() {
+
+//   const players = [
+//     {
+//       name: playerOneName,
+//       token: 1
+//     },
+//     {
+//       name: playerTwoName,
+//       token: 2
+//     }
+//   ];
+
+//   let activePlayer = players[0];
+
+//   const switchPlayerTurn = () => {
+//     activePlayer = activePlayer === players[0] ? players[1] : players[0];
+//   };
+//   const getActivePlayer = () => activePlayer;
+
+//   const printNewRound = () => {
+//     board.printBoard();
+//     console.log(`${getActivePlayer().name}'s turn.`);
+//   };
+
+//   const playRound = (column) => {
+//     // Drop a token for the current player
+//     console.log(
+//       `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+//     );
+//     board.dropToken(column, getActivePlayer().token);
+
+//     /*  This is where we would check for a winner and handle that logic,
+//         such as a win message. */
+
+//     // Switch player turn
+//     switchPlayerTurn();
+//     printNewRound();
+//   };
+// }
