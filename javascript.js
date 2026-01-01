51d6e869d492b5624ddbceed04a6152935183829
rac640 <50179707+rac640@users.noreply.github.com>
2025-12-31 22:21:12 -0500

    Created 2D array gameboard

--- javascript.js
+++ javascript.js
@@ -1,2 +1,23 @@
 // Use this as a guide: https://replit.com/@40percentzinc/ConnectFourConsole#script.js 
 
+
+
+
+
+//  created a 2D array: https://sentry.io/answers/how-can-i-create-a-two-dimensional-array-in-javascript/
+function Gameboard() {
+  const my2DArray = [];
+  const rows = 3;
+  const columns = 3;
+
+  for (let i = 0; i < rows; i++){
+    my2DArray[i] = [];
+    for (let j = 0; j < columns; j++){
+        my2DArray[i][j] = j;
+    }
+  }
+
+  console.log(my2DArray);
+}
+
+
