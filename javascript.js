841dc340833e80b1b309317821eeea984c7ba7ab
rac640 <50179707+rac640@users.noreply.github.com>
2026-01-05 20:13:03 -0500

    Simplified the gameboard so it's easier for me to understand

--- javascript.js
+++ javascript.js
@@ -8,108 +8,28 @@
 
 
 
+// Making a simple gameboard (2D Array): https://www.youtube.com/watch?v=SmJNeJuLmVo
+const gameBoard = [[1,2,3],
+                   [4,5,6],
+                   [7,8,9]]
 
-let  Gameboard = {
-     my2DArray: [],
-     rows : 3, 
-     columns : 3,
 
-     //  created a 2D array: https://sentry.io/answers/how-can-i-create-a-two-dimensional-array-in-javascript/
-  getArray: function(){
-  for (let i = 0; i < this.rows; i++){
-      this.my2DArray[i] = [];
-      for (let j = 0; j < this.columns; j++){
-          // element in row i, column j of my2DArray 
-          this.my2DArray[i][j] = j;
-      }
-    }
-    return this.my2DArray;
-  },
+// Attributes of player 1 object: Name, turn, and it can put an X on the board. 
+const player1 = {
+  name: "playerOne",
+  turn: true,
+  putAnX: function(){
+    // Gameboard.getArray() is an object type. 
+    console.log("testing:" + Gameboard.getArray());
+    // replace the 
 
-  putAnX: function(rowsInput,columnsInput){
-    this.my2DArray[rowsInput][columnsInput]="TEST";
-}
 
   }
 
-
-//  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-const player1 = {
-    name: "player1Name",
-    token: 1 ,
-    player1Value: "X"
-}
-
-const player2 = {
-    name: "player2Name", 
-    token: 2,
-    player2Value: "O"
 }
 
 
-// if the token is 1, then it is player1's turn. Put an X. 
 
 
 
 
-
-
-
-
-
-// below code taken from: https://replit.com/@40percentzinc/ConnectFourConsole#script.js
-// The GameController will be responsible for controlling the low and state of the game's turns, as well as whether anybody has won the game
-
-// function GameController() {
-
-//   const players = [
-//     {
-//       name: playerOneName,
-//       token: 1
-//     },
-//     {
-//       name: playerTwoName,
-//       token: 2
-//     }
-//   ];
-
-//   let activePlayer = players[0];
-
-//   const switchPlayerTurn = () => {
-//     activePlayer = activePlayer === players[0] ? players[1] : players[0];
-//   };
-//   const getActivePlayer = () => activePlayer;
-
-//   const printNewRound = () => {
-//     board.printBoard();
-//     console.log(`${getActivePlayer().name}'s turn.`);
-//   };
-
-//   const playRound = (column) => {
-//     // Drop a token for the current player
-//     console.log(
-//       `Dropping ${getActivePlayer().name}'s token into column ${column}...`
-//     );
-//     board.dropToken(column, getActivePlayer().token);
-
-//     /*  This is where we would check for a winner and handle that logic,
-//         such as a win message. */
-
-//     // Switch player turn
-//     switchPlayerTurn();
-//     printNewRound();
-//   };
-// }
